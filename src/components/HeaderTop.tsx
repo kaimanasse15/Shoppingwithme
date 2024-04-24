01ea23878caaa37720418bb6edb91ed9e152b74d
kaimanasse15 <91504156+kaimanasse15@users.noreply.github.com>
2024-04-23 21:01:31 -0700

    The components first part should be done

---
+++ src/components/HeaderTop.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { FaFacebook } from "react-icons/fa";
+import { IoLogoInstagram } from "react-icons/io5";
+import { FaYoutube } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+
+const HeaderTop = () => {
+  return (
+    <div className="border-b border-blue-200 hidden sm:block">
+      <div className="container py-4">
+        <div className="flex justify-center items-center">
+          <div className="header_top_logo_wrapper logo-wrapper">
+            <img src="logo.png" alt="Logo" width="50" height="60" />
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="text-gray-500 text-[12px]">
+            <b>50% Off</b> Free Shopping over $100
+          </div>
+          <div className="hidden lg:flex gap-1">
+            <div className="header_top_icon_wrapper">
+              <FaFacebook />
+            </div>
+            <div className="header_top_icon_wrapper">
+              <IoLogoInstagram />
+            </div>
+            <div className="header_top_icon_wrapper">
+              <FaYoutube />
+            </div>
+            <div className="header_top_icon_wrapper">
+              <FaTwitterSquare />
+            </div>
+            <div className="flex gap-4">
+              <select
+                className="text-gray-500 text-[12px] w-[70px]"
+                name="currency"
+                id="currency"
+              >
+                <option value="USD $"> USD $</option>
+                <option value="EUR $"> EUR $</option>
+              </select>
+              <select
+                className="text-gray-500 text-[12px] w-[70px]"
+                name="language"
+                id="language"
+              >
+                <option value="English"> English</option>
+                <option value="French"> French</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderTop
