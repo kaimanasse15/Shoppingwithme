d207304145c71295a4f12f4d5fd42f98afb9f63e
kaimanasse15 <91504156+kaimanasse15@users.noreply.github.com>
2024-04-29 11:13:31 -0700

    HeadMain has been done

--- src/components/HeaderMain.tsx
+++ src/components/HeaderMain.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { BiUser } from 'react-icons/bi';
+import { BsSearch } from 'react-icons/bs';
+import { FiHeart } from 'react-icons/fi';
+import { HiOutlineShoppingCart } from 'react-icons/hi';
 
 const HeaderMain = () => {
   return (
@@ -12,8 +16,27 @@ const HeaderMain = () => {
         <div className="w-full sm:w-[1000px] md:w-w[70%] relative">
           <input 
            className="border-gray-200 border p-2 px-4 rounded-lg w-full" type="text"
-           placeholder="Enter any product name..."
+           placeholder="Search Shoppingwithme.com"
            />
+               <BsSearch className="absolute right-0 top-0 mr-3 mt-3 text0-gray-400" size={20}/>
+
+        </div>
+        <div className="hidden lg:flex gap-4 text-gray-500 text-[30px]">
+          <BiUser/>
+          <div className="relative">
+          <FiHeart/>
+          <div className="bg-red-600 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[10px] 
+          text-white grid place-items-center translate-x-1 -translate-y-1">
+          0
+          </div>
+          </div>
+          <div className="relative">
+          <HiOutlineShoppingCart/>
+          <div className="bg-red-600 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[10px] 
+          text-white grid place-items-center translate-x-1 -translate-y-1">
+          0
+          </div>
+          </div>
         </div>
       </div>
     </div>
