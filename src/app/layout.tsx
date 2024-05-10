6bf120283389526e040a69615652a69796db0b25
kaimanasse15 <91504156+kaimanasse15@users.noreply.github.com>
2024-05-09 20:57:52 -0700

    New Product Page has been updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
         <HeaderTop/>
         <HeaderMain/>
         <Navbar/>
+        
         {children}
         </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import NewProducts from "@/components/NewProducts";
 import TopSlick from "@/components/TopSlick";
 
 export default function Home() {
   return (
     <main>
       <TopSlick/>
-
+      <NewProducts/>
       
     </main>
   );
